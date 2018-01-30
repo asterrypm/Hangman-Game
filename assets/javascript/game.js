37bf306ae12e9470bc1f48e5af47c052d20b70a6
asterrypm <asterrypm@gmail.com>
2018-01-30 17:15:49 -0500

    hangman javascript and basic HTML

---
+++ assets/javascript/game.js
@@ -0,0 +1,52 @@
+//GLOBAL VARIABLES
+//------------------------------------------------------------------------------------------------------------------
+
+var wordOptions = ["octopus", "shark", "dolphin", "eel", "lobster", "whale", "turtle", "jellyfish", "seal", "fish", "starfish", "seahorse", "anemone", "", ""];
+
+var wordAnswer = "";
+//selectedWord
+var letterCount = [];
+//lettersinWord
+var numofSpaces = 0;
+//numBlanks
+var lettersAndSpaces = [];
+//blanksAndSuccesses
+var wrongGuesses = [];
+
+var winCount = 0;
+
+var lossCount = 0;
+
+var guessLeft = 0;
+
+
+
+
+
+//FUNCTIONS
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+function gameStart () {
+	wordAnswer = 
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//PROCESS
+//--------------------------------------------------------------------------------------------------------------------------------
