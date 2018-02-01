5abb1263f66100cd23ea56af07731c2bde2c810f
asterrypm <asterrypm@gmail.com>
2018-02-01 10:22:40 -0500

    toubleshooting js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,29 +2,16 @@
 //------------------------------------------------------------------------------------------------------------------
 
 var wordOptions = ["octopus", "shark", "dolphin", "eel", "lobster", "whale", "turtle", "jellyfish", "seal", "fish", "starfish", "seahorse", "anemone", "", ""];
-
 var wordAnswer = "";
-//selectedWord
 var letterCount = [];
-//lettersinWord
 var numofSpaces = 0;
-//numBlanks
 var lettersAndSpaces = [];
-//blanksAndSuccesses
 var wrongGuesses = [];
-
 var letterGuessed = [];
-
-var isLetterInWord = [];
-
+var isLetterInWord = false;
 var winCount = 0;
-
 var lossCount = 0;
-
 var guessLeft = 0;
-
-
-
 
 
 //FUNCTIONS
@@ -66,30 +53,35 @@ function gameStart () {
 
 }
 
+//Set isLetterInWord to false for 
+function checkletters(letter) {
 
-function checkLetter(letter) {
-
-	for (var i=o; i<numofSpaces; i++) {
-		if(wordAnswer[i]) 
-	}
-}
-
-
-//Check if letter exists in the word and if then add letter to screen from array.
-if(isLetterInWord) {
+	isLetterInWord = false;
 	for (var i=0; i<numofSpaces; i++) {
-		if(wordAnswer[i] == letter) {
-			lettersAndSpaces[i] = letter;
+		if (wordAnswer[i] == letter) {
+			isLetterInWord = true; 
 		}
 	}
-}
 
-else {
-	wrongGuesses.push(letter);
-	guessLeft--
-}
+//Check if letter exists in the word and if then add letter to screen from array.
+	
+		for (var i=0; i<numofSpaces; i++) {
+			if(wordAnswer[i] == letter) {
+				lettersAndSpaces[i] = letter;
+				
+			}
+		}
+	
+
+	 	for (var i=0; i<numofSpaces; i++) {
+			if(wordAnswer[i] != letter) {
+				wrongGuesses.push(letter);
+			}
+		}		
 
 	console.log(lettersAndSpaces);
+	console.log(wrongGuesses);
+	
 
 function roundComplete () {
 	console.log("Win Count: " + winCount + " | Loss Count: " + lossCount  + " | Remaining Guesses: " + guessLeft);
@@ -100,39 +92,37 @@ function roundComplete () {
 	document.getElementById("wrongGuesses").innerHTML = wrongGuesses.join(" ");
 
 
-//Did player win?:
-if (lettersinWord.toString() == lettersAndSpaces.toString()) {
-	winCount++;
-	alert("You Win!");
+	//Did player win?:
+	if (lettersinWord.toString() == lettersAndSpaces.toString()) {
+		winCount++;
+		alert("You Win!");
 
-	//update Win count on screen
-	document.getElementId("winCount").innerHTML = winCount;
-	
-	startGame()
-}
+		//update Win count on screen
+		document.getElementId("winCount").innerHTML = winCount;
+		
+		startGame()
+	}
 
-	//Did player Lose?:
-else if (guessLeft == 0) {
-	lossCount++;
-	alert("You Lose!");
+		//Did player Lose?:
+	else if (guessLeft == 0) {
+		lossCount++;
+		alert("You Lose!");
 
-	//update Loss count on the screen
-	document.getElementById("lossCount").innerHTML = lossCount;
+		//update Loss count on the screen
+		document.getElementById("lossCount").innerHTML = lossCount;
 
-	startGame();
-}
+		startGame();
+	}
 
-}
 }
 	
 
 //PROCESS
 //--------------------------------------------------------------------------------------------------------------------------------
-startGame()
+gameStart()
 
 
-//To loop background music
-document.write('<audio loop autoplay="autoplay">'); 
+/*ocument.write('<audio loop autoplay="autoplay">'); */
 
 //Key clicks for Guesses
 document.onkeyup = function(event) {
@@ -142,41 +132,4 @@ document.onkeyup = function(event) {
 
 //Audio
 //---------------------------------------------------------------------------------------------------------------------------------
-$(document).ready(function() {
-    var audioElement = document.createElement('audio');
-    audioElement.setAttribute('src', 'http://www.soundjay.com/misc/sounds/bell-ringing-01.mp3');
-    
-    audioElement.addEventListener('ended', function() {
-        this.play();
-    }, false);
-    
-    audioElement.addEventListener("canplay",function(){
-        $("#length").text("Duration:" + audioElement.duration + " seconds");
-        $("#source").text("Source:" + audioElement.src);
-        $("#status").text("Status: Ready to play").css("color","green");
-    });
-    
-    audioElement.addEventListener("timeupdate",function(){
-        $("#currentTime").text("Current second:" + audioElement.currentTime);
-    });
-    
-   /* $('#play').click(function() {
-        audioElement.play();
-        $("#status").text("Status: Playing");
-    });
-    
-    $('#pause').click(function() {
-        audioElement.pause();
-        $("#status").text("Status: Paused");
-    });
-    
-    $('#restart').click(function() {
-        audioElement.currentTime = 0;
-    });
-});*/
 
-
-
-window.onload = function() {
-    document.getElementById("my_audio").play();
-}
